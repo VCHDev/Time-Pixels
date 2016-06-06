fb583e3f89fdf4f16e2d2930ad260554baa75bd2
VCHDev <vdronedeveloper@gmail.com>
2016-06-06 16:25:59 +0300

    refactored the classes and extracted the token and secretToken

--- Time Pixels/app/src/main/java/edu/todo/vch/timepixels/MainActivity.java
+++ Time Pixels/app/src/main/java/edu/todo/vch/timepixels/MainActivity.java
@@ -3,8 +3,6 @@ package edu.todo.vch.timepixels;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.io.UnsupportedEncodingException;
-
 import edu.todo.vch.timepixels.model.FlickrAutentificationClient;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,17 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FlickrAutentificationClient client = new FlickrAutentificationClient();
+        final FlickrAutentificationClient client = new FlickrAutentificationClient();
 
         String testUrl = null;
 
-        try {
-            testUrl = client.createTokenRequest();
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                client.autentificate();
+            }
+        });
+
+        t.start();
 
 
-            System.out.println("-----------------> 1 " + testUrl);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
     }
 }
--- Time Pixels/app/src/main/java/edu/todo/vch/timepixels/model/FlickrAutentificationClient.java
+++ Time Pixels/app/src/main/java/edu/todo/vch/timepixels/model/FlickrAutentificationClient.java
@@ -4,13 +4,19 @@ package edu.todo.vch.timepixels.model;
  * Created by Valentin on 30/05/16.
  */
 
-import android.net.Uri;
 import android.util.Base64;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.crypto.Mac;
@@ -23,15 +29,47 @@ public class FlickrAutentificationClient {
     private static final String CONSUMER_API_KEY = "789f557cecdbf7438a540d518485589b";
     private static final String CONSUMER_API_SECRET = "af54f2a63f266a8e";
     private static final String ISO_8859_1 = "ISO-8859-1";
+    private static final String HMAC_SHA1 = "HmacSHA1";
+
+
+    private static final class Oauth {
+        private static final String CALLBACK = "oauth_callback";
+        private static final String CONSUMER_KEY = "oauth_consumer_key";
+        private static final String NONCE = "oauth_nonce";
+        private static final String SIGNATURE_METHOD = "oauth_signature_method";
+        private static final String TIMESTAMP = "oauth_timestamp";
+        private static final String VERSION = "oauth_version";
+        private static final String SIGNATURE = "oauth_signature";
+    }
+
+    private static final class Uri {
+        private static final String SCHEME = "http";
+        private static final String AUTHORITY = "www.flickr.com";
+        private static final String SERVICES = "services";
+        private static final String OAUTH = "oauth";
+        private static final String REQUEST_TOKEN = "request_token";
+    }
+
+
+    private static final class OauthParams {
+        private static final String VERSION = "1.0";
+        private static final String CALLBACK_ADDRESS = "http://www.vhcdev.edu/oauth/test";
+        private static final String SIGNATURE = "HMAC-SHA1";
+
+    }
+
+
     private String tokenSecret;
     private String oauthNonce;
     private String timeStamp;
+    private String token;
 
 
     public FlickrAutentificationClient() {
         this.oauthNonce = generateOauthNonce();
         this.timeStamp = generateTimeStamp();
         tokenSecret = "";
+        token = "";
     }
 
 
@@ -44,43 +82,43 @@ public class FlickrAutentificationClient {
     }
 
     public String createSigningRequest() throws UnsupportedEncodingException {
-        return createSignedURI(createURIPath(), createURIParameters().build().toString());
+        return createSignedURI(createURIPath(), createURIParam().build().toString());
     }
 
     private String createURIPath() {
-        Uri.Builder uriPath = new Uri.Builder();
-        uriPath.scheme("http")
-                .authority("www.flickr.com")
-                .appendPath("services")
-                .appendPath("oauth")
-                .appendPath("request_token");
+        android.net.Uri.Builder uriPath = new android.net.Uri.Builder();
+        uriPath.scheme(Uri.SCHEME)
+                .authority(Uri.AUTHORITY)
+                .appendPath(Uri.SERVICES)
+                .appendPath(Uri.OAUTH)
+                .appendPath(Uri.REQUEST_TOKEN);
 
         return uriPath.build().toString();
     }
 
-    private Uri.Builder createURIParameters() throws UnsupportedEncodingException {
-        Uri.Builder uriParam = new Uri.Builder()
-                .appendQueryParameter("oauth_callback", "http://www.vhcdev.edu/oauth/test")
-                .appendQueryParameter("oauth_consumer_key", CONSUMER_API_KEY)
-                .appendQueryParameter("oauth_nonce", oauthNonce)
-                .appendQueryParameter("oauth_signature_method", "HMAC-SHA1")
-                .appendQueryParameter("oauth_timestamp", timeStamp)
-                .appendQueryParameter("oauth_version", "1.0");
+    private android.net.Uri.Builder createURIParam() throws UnsupportedEncodingException {
+        android.net.Uri.Builder uriParam = new android.net.Uri.Builder()
+                .appendQueryParameter(Oauth.CALLBACK, OauthParams.CALLBACK_ADDRESS)
+                .appendQueryParameter(Oauth.CONSUMER_KEY, CONSUMER_API_KEY)
+                .appendQueryParameter(Oauth.NONCE, oauthNonce)
+                .appendQueryParameter(Oauth.SIGNATURE_METHOD, OauthParams.SIGNATURE)
+                .appendQueryParameter(Oauth.TIMESTAMP, timeStamp)
+                .appendQueryParameter(Oauth.VERSION, OauthParams.VERSION);
 
         return uriParam;
+    }
+
+    private String createURIParamForRequestToken(android.net.Uri.Builder data, String oauthSignature) throws UnsupportedEncodingException {
+        data.appendQueryParameter(Oauth.SIGNATURE, oauthSignature);
+        return data.build().toString();
     }
 
     private String encodeString(String data) throws UnsupportedEncodingException {
         return URLEncoder.encode(data, ISO_8859_1);
     }
 
-    private String createURIParametersForRequestToken(Uri.Builder data, String oauthSignature) throws UnsupportedEncodingException {
-        data.appendQueryParameter("oauth_signature", oauthSignature);
-        return data.build().toString();
-    }
-
-    private String createSignedURI(String uripath, String uriParameters) throws UnsupportedEncodingException {
-        return "GET" + "&" + encodeString(uripath) + "&" + encodeString(uriParameters.replace("?", ""));
+    private String createSignedURI(String uriPath, String uriParameters) throws UnsupportedEncodingException {
+        return "GET" + "&" + encodeString(uriPath) + "&" + encodeString(uriParameters.replace("?", ""));
     }
 
     private String createURIForRequestToken(String path, String parameters) {
@@ -89,7 +127,7 @@ public class FlickrAutentificationClient {
 
     public String createTokenRequest() throws UnsupportedEncodingException {
         String signedURI = generateHMACSha1(createSigningRequest(), createSecretKey());
-        String urlParametersForRequestToken = createURIParametersForRequestToken(createURIParameters(), signedURI);
+        String urlParametersForRequestToken = createURIParamForRequestToken(createURIParam(), signedURI);
         return createURIForRequestToken(createURIPath(), urlParametersForRequestToken);
     }
 
@@ -106,12 +144,78 @@ public class FlickrAutentificationClient {
     }
 
     private String hashURIForRequestToken(String data, String secretKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
-        Mac mac = Mac.getInstance("HmacSHA1");
+        Mac mac = Mac.getInstance(HMAC_SHA1);
         SecretKeySpec secret = new SecretKeySpec(secretKey.getBytes("UTF-8"), mac.getAlgorithm());
         mac.init(secret);
         byte[] digest = mac.doFinal(data.getBytes("UTF-8"));
 
         return new String(Base64.encodeToString(digest, Base64.NO_WRAP));
+    }
+
+
+    public void autentificate() {
+        try {
+            getRequestToken(createTokenRequest());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    private void getRequestToken(String uri) {
+        HttpURLConnection urlConnection  = null;
+        try {
+            urlConnection = getHttpURLConnectionToAccesRequestTokens(uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+    }
+
+ 
+    private HttpURLConnection getHttpURLConnectionToAccesRequestTokens(String uri) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) new URL(uri).openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String line;
+        String responseString = "";
+
+        while (( line = reader.readLine()) != null )
+            responseString += line;
+
+        getTokensFromResponse(responseString);
+
+        return urlConnection;
+    }
+
+    private void getTokensFromResponse(String responseString) {
+        String[] tokens = responseString.split("&");
+        Map<String, String> tokenMap = new HashMap<>();
+        extractTokensFromStringArrayToKeyValueMap(tokens, tokenMap);
+        extractTokenAndSecretTokenFromMap(tokenMap);
+
+        System.out.println("----- tokenSecret " + tokenSecret + " --- token === " + token);
+    }
+
+    private void extractTokenAndSecretTokenFromMap(Map<String, String> tokenMap) {
+        for (Map.Entry<String, String> entry : tokenMap.entrySet()) {
+            System.out.println(entry.getKey() + "//" + entry.getValue());
+
+            if (entry.getKey().equals("oauth_token_secret") ) {
+                tokenSecret = entry.getValue();
+            } else if (entry.getKey().equals("oauth_token")) {
+                token = entry.getValue();
+            }
+        }
+    }
+
+    private void extractTokensFromStringArrayToKeyValueMap(String[] tokens, Map<String, String> tokenMap) {
+        for (String tokenLine: tokens) {
+            String[] keYValue = tokenLine.split("=");
+            tokenMap.put(keYValue[0], keYValue[1]);
+        }
     }
 
 
