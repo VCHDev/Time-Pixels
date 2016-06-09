b5e93c93cd42af386f230c00e3a4d311f944ec3a
VCHDev <vdronedeveloper@gmail.com>
2016-06-09 15:56:14 +0300

    implemented page indicator for tutorial view pager

--- Time Pixels/app/src/main/java/edu/todo/vch/timepixels/authenticate/view/AuthenticateFragment.java
+++ Time Pixels/app/src/main/java/edu/todo/vch/timepixels/authenticate/view/AuthenticateFragment.java
@@ -1,14 +1,18 @@
 package edu.todo.vch.timepixels.authenticate.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -22,6 +26,10 @@ import edu.todo.vch.timepixels.authenticate.adapter.AuthenticateInfoPageAdapter;
 public class AuthenticateFragment extends Fragment {
     private static final String TAG = AuthenticateFragment.class.getSimpleName();
 
+    private LinearLayout pageDotIndicatorLayout;
+    private ImageView[] dotsPageIndicators;
+    private int dotsCount;
+
     private AuthenticateInfoPageAdapter infoPageAdapter;
 
     @Override
@@ -29,6 +37,8 @@ public class AuthenticateFragment extends Fragment {
         super.onCreate(savedInstanceState);
         String[] infoData = getResources().getStringArray(R.array.authenticateInfoArray);
         infoPageAdapter = new AuthenticateInfoPageAdapter(getActivity().getSupportFragmentManager(), infoData);
+        dotsCount = infoPageAdapter.getCount();
+        dotsPageIndicators = new ImageView[dotsCount];
 
         Log.d(TAG, "array data " + Arrays.toString(infoData));
     }
@@ -41,14 +51,36 @@ public class AuthenticateFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        pageDotIndicatorLayout = (LinearLayout) view.findViewById(R.id.view_page_indicator);
+
         Button signInAppBtn = (Button) view.findViewById(R.id.sign_in_button);
         Button joinAppBtn = (Button) view.findViewById(R.id.join_button);
 
         ViewPager infoPager = (ViewPager) view.findViewById(R.id.info_pager);
         infoPager.setAdapter(infoPageAdapter);
+        infoPager.addOnPageChangeListener(new ViewPagerChangeListener());
+        buildUIPageIndicator();
 
         signInAppBtn.setOnClickListener(new SignInAppBtnListener());
         joinAppBtn.setOnClickListener(new JoinAppBtnListener());
+    }
+
+    private void buildUIPageIndicator(){
+        for (int i = 0 ; i < dotsCount; i++) {
+            dotsPageIndicators[i] = new ImageView(getContext());
+            dotsPageIndicators[i].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.unselected_dot_item, null));
+
+            pageDotIndicatorLayout.addView(dotsPageIndicators[i], createLayoutParams());
+        }
+
+        dotsPageIndicators[0].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.selected_dot_item, null));
+    }
+
+    @NonNull
+    private LinearLayout.LayoutParams createLayoutParams() {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(12, 0, 12, 0);
+        return params;
     }
 
     private class SignInAppBtnListener implements View.OnClickListener{
@@ -69,5 +101,26 @@ public class AuthenticateFragment extends Fragment {
 
     private void createToast(String message) {
         Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private class ViewPagerChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            for (int i = 0 ; i <  dotsCount; i++)
+                dotsPageIndicators[i].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.unselected_dot_item, null));
+
+            dotsPageIndicators[position].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.selected_dot_item, null));
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }
